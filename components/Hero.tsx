e49c8e4104173239a23ad9c6629324d9e917291d
ansh <anshsrivastava112006@gmail.com>
2026-08-06 14:35:27 +0530

    hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,21 @@
+export default function Hero(){
+    return(
+        <section className="max-w-7x1 mx-auto px-6">
+            <div className="min-h-[80vh] flex item-center">
+                <div className="flex justify-between items-center w-full">
+                    <div className="inline-flex items-center gap-2 rounded-full bg-purple-100 px-4 py-2">
+                        <span className="text-sm font-medium text-purple-800">
+                            Elevate Your Tommorow
+                        </span>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+    );
+}
