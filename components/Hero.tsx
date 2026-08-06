97514ab0b25d488ad5bc2b7eedf6157f7e3be0e4
ansh <anshsrivastava112006@gmail.com>
2026-08-06 16:06:09 +0530

    buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function Hero() {
     return (
         <section className="max-w-7xl mx-auto px-6">
@@ -26,6 +27,17 @@ export default function Hero() {
                             Learn, build, and grow with in-depth articles, AI tools,
                             real-world projects, and the latest tech insights.
                         </p>
+                        <div className="mt-8 flex items-center gap-4">
+                            <button>
+                                <Link href="/articles"className="rounded-xl bg-purple-700 px-6 py-3 text-white font-semibold shadow-lg hover:bg-purple-800 transition-colors duration-200">
+                                Explore Articles →
+                                </Link>
+
+                                <Link href="/tools" className="rounded-xl border border-gray-300 bg-white px-6 py-3 font-semibold text-gray-900 hover:border-purple-700 hover:text-purple-700 transition-colors duration-200">
+                                Explore AI Tools
+                                </Link>
+                            </button>
+                        </div>
 
                     </div>
 
