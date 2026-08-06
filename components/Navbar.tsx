7f63d16ac859cec1645de99d9c14abc5e513a537
ansh <anshsrivastava112006@gmail.com>
2026-08-06 14:13:09 +0530

    links and hover

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export default function Navbar(){
     const navItems=[
-        "Articles",
-        "AI",
-        "System Design",
-        "Tools",
-        "News",
-    ]
+         { name: "Articles", href: "/articles" },
+         { name: "AI", href: "/ai" },
+         { name: "System Design", href: "/system-design" },
+         { name: "Tools", href: "/tools" },
+         { name: "News", href: "/news" },
+        ]
   
     return(
-        <nav>
+        <nav className="sticky top-0 bg-white border-b border-gray-200 z-50">
             <div className="max-w-7x1 mx-auto flex justify-between items-center h-16 px-6">
                 <div>
                     <Link href="/" className="flex items-center gap-2">
@@ -23,8 +23,8 @@ export default function Navbar(){
                     </Link>
                 </div>
                 <div className="flex gap-8">
-                    {navItems.map((item)=>(<Link key={item} href="#">
-                    {item}
+                    {navItems.map((item)=>(<Link key={item.name} href={item.href} className="hover:text-purple-700 transition-colors duration-200">
+                    {item.name}
                     </Link>))}
                     Links
                 </div>
