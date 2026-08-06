4c28cc9683fbbb547c0c96ef2c7bd3aa5d83c594
ansh <anshsrivastava112006@gmail.com>
2026-08-06 09:03:13 +0530

    Elvoret logo config

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,10 +4,13 @@ import Image from "next/image";
 export default function Navbar(){
     return(
         <nav>
-            <div className="max-w-7x1 mx-auto flex justify-between items-center h-16">
+            <div className="max-w-7x1 mx-auto flex justify-between items-center h-16 px-6">
                 <div>
-                    <Link href="/">
+                    <Link href="/" className="flex items-center gap-2">
                     <Image src="/logo.png" alt="Elvoret Logo" width={40} height={40} />
+                    <span className="text-xl font-bold tracking wide text-purple-900">
+                        ELVORET
+                    </span>
 
                     </Link>
                 </div>
