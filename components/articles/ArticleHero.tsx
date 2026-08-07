20952feaabd1fce20e655bf5e2b787d796bdac7c
ansh <anshsrivastava112006@gmail.com>
2026-08-07 12:41:36 +0530

    articles

---
+++ components/articles/ArticleHero.tsx
@@ -0,0 +1,18 @@
+export default function ArticleHero(){
+    return(
+        <section className="max-w-[1440px] mx-auto px-6 py-16">
+            <div>
+                <div className="flex flex-xol">
+                    <div className="inline-flex items-center gap-2 self-start rounded-full bg-purple-100 px-4 py-2">
+                        <span className="text-sm font-medium text-purple-800">
+                            Explore our Knowledge Base
+                        </span>
+                        <h1 className="mt-6 text-6xl font-extrabold tracking-tight text-gray-900">
+                            Articles
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
