04a4469ad0591d9e9b59112f02100911e869eee9
ansh <anshsrivastava112006@gmail.com>
2026-08-07 16:03:45 +0530

    hell-1

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -8,7 +8,7 @@ export default function ArticlesPage(){
     return(
         <>
         <Navbar />
-        <section className="max-w-[1440px] mx-auto px-6 py-16">
+        <section className="max-w-[1440px] mx-auto px-6 py-10">
             <div className="flex gap-10">
                 <main className="flex-1 min-w-0">
                     <ArticleHero />
