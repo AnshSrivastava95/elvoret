49b0f43566138dc12d7faf042bdc910c174b4e76
ansh <anshsrivastava112006@gmail.com>
2026-08-07 13:37:55 +0530

    gf

--- components/articles/ArticleHero.tsx
+++ components/articles/ArticleHero.tsx
@@ -1,7 +1,7 @@
 export default function ArticleHero(){
     return(
-        <section className="max-w-[1440px] mx-auto px-6 py-16">
-            <div>
+        <section className="w-full py-16">
+            <div className="w-full">
                 <div className="flex flex-col">
                     <div className="inline-flex items-center gap-2 self-start rounded-full bg-purple-100 px-4 py-2">
                         <span className="text-sm font-medium text-purple-800">
@@ -11,7 +11,7 @@ export default function ArticleHero(){
                     <h1 className="mt-6 text-6xl font-extrabold tracking-tight text-gray-900">
                             Articles
                     </h1>
-                    <p className="mt-6 max-w-3xl text-xl leading-9 text-gray-600">
+                    <p className="mt-6 max-w-[850px] text-xl leading-9 text-gray-600">
                         Practical tutorials, AI, System Design, Backend Development,
                         DSA, career advice, and developer resources—all in one place.
                     </p>
