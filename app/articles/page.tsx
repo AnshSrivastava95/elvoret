7424205f2c71fa6eca3e84ca0c1193300dc0cd2b
ansh <anshsrivastava112006@gmail.com>
2026-08-07 12:53:14 +0530

    articles

---
+++ app/articles/page.tsx
@@ -0,0 +1,8 @@
+import ArticlesHero from "@/components/articles/ArticlesHero";
+export default function ArticlesPage(){
+    return(
+        <main>
+            <ArticlesHero/>
+        </main>
+    );
+}
