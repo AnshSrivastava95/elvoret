638d6e4508c7b0743d659c72ce2d01b1ff588897
ansh <anshsrivastava112006@gmail.com>
2026-08-07 13:31:46 +0530

    cm

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,16 +1,23 @@
-import ArticlesHero from "@/components/articles/ArticleHero";
+import ArticleHero from "@/components/articles/ArticleHero";
 import Navbar from "@/components/layout/Navbar";
 import SearchBar from "@/components/ui/SearchBar";
 import CategoryCards from "@/components/articles/CategoryCard";
 export default function ArticlesPage(){
     return(
         <>
-        <Navbar/>
-        <main>
-            <ArticlesHero/>
-            <SearchBar/>
-            <CategoryCards/>
-        </main>
+        <Navbar />
+        <section className="max-w-[1440px] mx-auto px-6 py-16">
+            <div className="flex gap-10">
+                <main className="w-[72%]">
+                    <ArticleHero />
+                    <SearchBar />
+                    <CategoryCards />
+                </main>
+                <aside className="w-[28%]">
+
+                </aside>
+            </div>
+        </section>
         </>
     );
 }
