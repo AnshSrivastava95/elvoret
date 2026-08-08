60512f0903375cbdcbf16933c9fae698c3f3b8e5
ansh <anshsrivastava112006@gmail.com>
2026-08-08 14:11:50 +0530

    pad

--- components/home/CategoryCard.tsx
+++ components/home/CategoryCard.tsx
@@ -50,7 +50,7 @@ const categories = [
 
 export default function CategoryCards() {
   return (
-    <section className="w-full py-16 sm:py-20">
+    <section className="w-full py-16 sm:py-10">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 
         {/* Section Header */}
